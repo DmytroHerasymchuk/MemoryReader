36278facc791ba9691401fe403186acfc4496d33
Дмитро Герасимчук <dmytro.herasymchuk@nure.ua>
2022-12-29 00:19:12 +0200

    Добавьте файлы проекта.

---
+++ TermWork/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+namespace TermWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static readonly Regex _addressPattern = new Regex("0[xX][0-9a-fA-F]+");
+        private static readonly Regex _onlyNums = new Regex("[^0-9.-]+");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Text = MemoryReader.Read(Convert.ToInt32(PIDTB.Text), Convert.ToInt32(AddressTB.Text), Convert.ToInt32(CountTB.Text));
+        }
+
+
+
+
+        
+    
+        private static bool IsTextAllowed(string text)
+        {
+            return !_onlyNums.IsMatch(text);
+        }
+        private static bool IsAddressAllowed(string text)
+        {
+            return !_addressPattern.IsMatch(text);
+        }
+
+
+        private void PIDTB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void AddressTB_PreviewTextInput_1(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsAddressAllowed(e.Text);
+        }
+    }
+}
---
+++ TermWork/MemoryReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace TermWork
+{
+    public class MemoryReader
+    {
+        public List<Process> Processes = new List<Process>();
+
+        const int PROCESS_WM_READ = 0x0010;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess
+             (int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory
+        (int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        public static string Read(int PID, int address, int countOfSymbols)
+        {
+            Process process = Process.GetProcessById(PID);
+            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[countOfSymbols * 2];
+
+
+
+            ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+            string result;
+            result = BitConverter.ToString(buffer);
+            return result;
+        }
+    }
+}
